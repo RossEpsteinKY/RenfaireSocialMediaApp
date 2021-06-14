05ae93c2bae96b6176dc783f154711c7600dead6
RossEpsteinKY <ross.epstein@hotmail.com>
2021-06-14 16:36:57 -0400

    Refreshed db, added logindto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -42,5 +42,24 @@ namespace API.Controllers
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invalid username or password.");
+            
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid username or password.");
+            }
+
+            return user;
+        }
     }
 }
