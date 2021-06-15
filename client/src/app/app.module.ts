704cd6864b96d3652b41850605ad7111ecde7ed9
RossEpsteinKY <ross.epstein@hotmail.com>
2021-06-15 11:00:41 -0400

    added registration form wireframing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,13 +8,15 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {FormsModule} from "@angular/forms";
 import {BsDropdownModule} from "ngx-bootstrap/dropdown";
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
