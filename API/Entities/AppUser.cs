d38fd6e85300e58633cee4b44eb98338fbadf59a
RossEpsteinKY <ross.epstein@hotmail.com>
2021-06-16 12:42:55 -0400

    expanded users entity, added photos entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace API.Entities
 {
     public class AppUser
@@ -9,6 +13,38 @@ namespace API.Entities
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string Gender { get; set; }
+
+        public string Pronouns { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string Interests { get; set; }
+
+        public string HomeFaire { get; set; }
+
+        public string ActType { get; set; }
+
+        public string City { get; set; }
+        
+        public string State { get; set; }
+        
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
+
+
+
 
     }
 }
