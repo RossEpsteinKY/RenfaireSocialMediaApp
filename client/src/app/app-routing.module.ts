1874212837380a5e78414d327eab3362dcd31bbf
RossEpsteinKY <ross.epstein@hotmail.com>
2021-06-23 09:40:22 -0400

    User edit component gets user, changed site title to working title renbook

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import {AuthGuard} from "./_guards/auth.guard";
 import {TestErrorsComponent} from "./errors/test-errors/test-errors.component";
 import {NotFoundComponent} from "./errors/not-found/not-found.component";
 import {ServerErrorComponent} from "./errors/server-error/server-error.component";
+import {MemberEditComponent} from "./members/member-edit/member-edit.component";
 
 const routes: Routes = [
   //Non-Protected Routes
@@ -20,6 +21,7 @@ const routes: Routes = [
     children: [
       { path: 'members', component: MemberListComponent},
       { path: 'members/:username', component: MemberDetailComponent },
+      { path: 'member/edit', component: MemberEditComponent },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
     ]
--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Member} from "../../Models/member";
+import {User} from "../../Models/User";
+import {AccountService} from "../../_services/account.service";
+import {MembersService} from "../../_services/members.service";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'app-member-edit',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  member:Member;
+  user:User;
 
-  constructor() { }
+
+  constructor(private accountService : AccountService, private memberService : MembersService) {
+
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+  }
 
   ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    this.memberService.getMember(this.user.username).subscribe(member => this.member = member);
   }
 
 }
